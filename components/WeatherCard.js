b9784b0887f91e85e65e56513bd55a589a9181de
Vitalie <138406664+Vitalie-Dragan@users.noreply.github.com>
2023-08-16 02:32:28 +0100

    Map in React

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,20 +1,22 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import sunny from "../assets/Sunny.svg";
+// import cities from "../data";
 
 
-function WeatherCard() {
+function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
             <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">{props.cities.city}</h3>
+            <h5 className="card-text">{props.cities.temperature}</h5>
+            <h5 className="card-text">{props.cities.forecast}</h5>
         </div>
     </div>
   );
 };
 
 // Export the WeatherCard
+export default WeatherCard
